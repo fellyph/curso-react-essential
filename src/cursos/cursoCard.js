b5d12aa859ba609a72e59f55f9cfdb8111ab66e1
fellyph <fellyph.cintra@gmail.com>
2018-11-19 14:29:08 +0000

    adding card module

--- src/cursos/cursoCard.js
+++ src/cursos/cursoCard.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import FavoritarBtn from '../shared/favoritarBtn';
+import estilo from './cursoCard.module.css';
 
 export class CursoCard extends React.Component {
   listaRequistos = this.props.requisitos.map((tag, index) => <li key={index}>{tag}</li>);
   render() {
-    return <article className="course presencial">
-            <div className="card">
-                <img className="responsive-img" src={this.props.thumb} alt={this.props.nome} />
-                <div className="card-block">
-                    <time className="card-date">{this.props.dataCurso}</time>
-                    <h4 className="card-title">{this.props.nome}</h4>
-                    <p className="card-text">{this.props.categoria}</p>
-                    <h5>Pre-requisitos:</h5>
-                    <ul className="card-tags">
-                      {this.listaRequistos}
-                    </ul>
-                    <FavoritarBtn />
-                </div>
+    return <article className={estilo.card}>
+            <img className={estilo.card__img} src={this.props.thumb} alt={this.props.nome} />
+            <div className={estilo.card__block}>
+                <time className={estilo.card__date}>{this.props.dataCurso}</time>
+                <h4 className={estilo.card__title}>{this.props.nome}</h4>
+                <p className={estilo.card__text}>{this.props.categoria}</p>
+                <h5 className={estilo.card__subtitle}>Pre-requisitos:</h5>
+                <ul className={estilo.card__tags}>
+                  {this.listaRequistos}
+                </ul>
+                <FavoritarBtn />
             </div>
           </article>;
   }
